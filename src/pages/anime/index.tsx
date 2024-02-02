eff8ab0621ff0e1c8cb64ac9cc8febc89e0e24f4
Ming Lee Ng <mingleeng@gmail.com>
2024-02-02 13:25:31 -0700

    chore: updated main page with placeholder information
    - Enhanced main page by adding placeholder info

--- src/pages/anime/index.tsx
+++ src/pages/anime/index.tsx
@@ -1,13 +1,15 @@
-import { useSession, signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data } = useSession();
 
   return (
     <div>
-      Hello there {data?.user.name}
-      <br />
-      <button onClick={() => signOut()}>SIGNOUT</button>
+      <h1>Hello there {data?.user.name}</h1>
+      <h2>
+        Welcome to the main page! There will be important stuff here,
+        eventually. I think. Hopefully.
+      </h2>
     </div>
   );
 }
