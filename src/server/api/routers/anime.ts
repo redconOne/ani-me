8504276e5eaa5feeb7c59bc0162f526dd21e5e9d
Ming Lee Ng <mingleeng@gmail.com>
2024-02-03 22:44:23 -0700

    feat: add top anime page and api endpoint

--- src/server/api/routers/anime.ts
+++ src/server/api/routers/anime.ts
@@ -2,6 +2,32 @@ import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { type Anime, type SearchResponse } from "@/types/anime";
 import { z } from "zod";
 
+const generateAnimeFromResponse = (arr: Anime[], animeList: SearchResponse) => {
+  for (const item of animeList.data) {
+    const anime: Anime = {
+      mal_id: item.mal_id,
+      default_title: "",
+      english_title: "",
+      trailer: item.trailer.url || "",
+      type: item.type ?? "",
+      image: item.images.jpg.image_url || "",
+      status: item.status || "",
+      rating: item.rating || "",
+      score: item.score || 0,
+      rank: item.rank || 0,
+      synopsis: item.synopsis || "No Synopsis Available",
+    };
+
+    for (const obj of item.titles) {
+      if (obj.type === "Default" || !anime.default_title)
+        anime.default_title = obj.title;
+      if (obj.type === "English") anime.english_title = obj.title;
+    }
+
+    arr.push(anime);
+  }
+};
+
 export const animeRouter = createTRPCRouter({
   search: protectedProcedure
     .input(z.object({ name: z.string(), sfw: z.boolean() }))
@@ -17,31 +43,29 @@ export const animeRouter = createTRPCRouter({
           `Failed to fetch data from API. Status: ${apiResponse.status}`,
         );
 
-      const apiResults = (await apiResponse.json()) as SearchResponse;
+      const parsedResponse = (await apiResponse.json()) as SearchResponse;
 
-      for (const item of apiResults.data) {
-        const anime: Anime = {
-          mal_id: item.mal_id,
-          default_title: "",
-          english_title: "",
-          trailer: item.trailer.url || "",
-          type: item.type ?? "",
-          image: item.images.jpg.image_url || "",
-          status: item.status || "",
-          rating: item.rating || "",
-          score: item.score || 0,
-          rank: item.rank || 0,
-          synopsis: item.synopsis || "No Synopsis Available",
-        };
+      generateAnimeFromResponse(response, parsedResponse);
 
-        for (const obj of item.titles) {
-          if (obj.type === "Default" || !anime.default_title)
-            anime.default_title = obj.title;
-          if (obj.type === "English") anime.english_title = obj.title;
-        }
+      return response;
+    }),
 
-        response.push(anime);
-      }
+  top: protectedProcedure
+    .input(z.object({ limit: z.number() }))
+    .query(async ({ input }) => {
+      const response: Anime[] = [];
+      const { limit } = input;
+      const url = `${process.env.API_URL}top/anime?limit=${limit}`;
+      const apiResponse = await fetch(url);
+
+      if (!apiResponse.ok)
+        throw new Error(
+          `Failed to fetch data from API. Status: ${apiResponse.status}`,
+        );
+
+      const parsedResponse = (await apiResponse.json()) as SearchResponse;
+
+      generateAnimeFromResponse(response, parsedResponse);
 
       return response;
     }),
