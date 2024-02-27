ddebc3ca6a3726bd642e47ee1f48d2e75f5e2222
Ming Lee Ng <mingleeng@gmail.com>
2024-02-27 11:01:44 -0700

    feat: added watchedlist toggler mutation

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -8,18 +8,12 @@ export const userRouter = createTRPCRouter({
     .query(async ({ ctx, input }) => {
       const id = ctx.session.user.id;
       const { type } = input;
+      const wishlist = type === "wishlist" || type === "all";
+      const watchedlist = type === "watchedlist" || type === "all";
 
-      if (type === "wishlist") {
-        const dbResponse = (await ctx.db.user.findUnique({
-          where: { id },
-          include: { wishlist: true },
-        })) as ListResponse;
-
-        return dbResponse;
-      }
       const dbResponse = (await ctx.db.user.findUnique({
         where: { id },
-        include: { watchlist: true },
+        include: { watchedlist: watchedlist, wishlist: wishlist },
       })) as ListResponse;
 
       return dbResponse;
@@ -102,6 +96,92 @@ export const userRouter = createTRPCRouter({
           where: { id },
           data: {
             wishlist: {
+              disconnect: [{ mal_id }],
+            },
+          },
+        });
+    }),
+
+  toggleWatchedlist: protectedProcedure
+    .input(
+      z.object({
+        mal_id: z.number(),
+        default_title: z.string(),
+        english_title: z.string(),
+        trailer: z.string(),
+        image: z.string(),
+        type: z.string(),
+        status: z.string(),
+        synopsis: z.string(),
+        rating: z.string(),
+        score: z.number(),
+        rank: z.number(),
+        add: z.boolean(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const {
+        mal_id,
+        default_title,
+        english_title,
+        trailer,
+        image,
+        type,
+        status,
+        synopsis,
+        rating,
+        score,
+        rank,
+        add,
+      } = input;
+      const id = ctx.session.user.id;
+      await ctx.db.anime.upsert({
+        where: { mal_id },
+        create: {
+          mal_id,
+          default_title,
+          english_title,
+          trailer,
+          image,
+          type,
+          status,
+          rating,
+          score,
+          rank,
+          synopsis,
+        },
+        update: {
+          mal_id,
+          default_title,
+          english_title,
+          trailer,
+          image,
+          type,
+          status,
+          rating,
+          score,
+          rank,
+          synopsis,
+        },
+      });
+
+      if (add)
+        await ctx.db.user.update({
+          where: { id },
+          data: {
+            watchedlist: {
+              connect: [{ mal_id }],
+            },
+            wishlist: {
+              disconnect: [{ mal_id }],
+            },
+          },
+        });
+      else
+        await ctx.db.user.update({
+          where: { id },
+          data: {
+            watchedlist: {
               disconnect: [{ mal_id }],
             },
           },
